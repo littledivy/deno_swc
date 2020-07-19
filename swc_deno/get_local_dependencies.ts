09740f5a9bd459b2b9fe659440c3a89249e32cf2
divy-work <dj.srivastava23@gmail.com>
2020-07-19 15:45:13 +0530

    feat: add types and exports

--- swc_deno/get_local_dependencies.ts
+++ swc_deno/get_local_dependencies.ts
@@ -1,6 +1,12 @@
+import {
+  swc_analyze,
+} from "../swc_plugin/index.ts";
+
+import { AnalyzeOptions } from "../types/options.ts";
+
 /**
  * Get the local dependencies of a Typescript/Javascript file, excluding remote dependencies.
  */
-export const getLocalDependencies = (filename: string): string[] => {
-  throw new Error(`Unimplemented yet`);
+export const getLocalDependencies = (opt: AnalyzeOptions): string[] => {
+
 };
