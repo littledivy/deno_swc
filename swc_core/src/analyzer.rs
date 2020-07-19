dc35d3da78bf47bfbb316ffe4e5fa17fbc04a096
Wong Jia Hau <hou32hou@gmail.com>
2020-07-19 22:20:12 +0800

    Improve analyzer.rs

--- swc_core/src/analyzer.rs
+++ swc_core/src/analyzer.rs
@@ -27,35 +27,28 @@ impl Visit for DependencyVisitor {
     }
 
     fn visit_call_expr(&mut self, call_expr: &CallExpr, _parent: &dyn Node) {
-        use swc_ecma_ast::{Expr::*, ExprOrSuper::*, Lit};
+        use swc_ecma_ast::{Expr::*, ExprOrSpread, ExprOrSuper::*, Ident, Lit::Str};
         if !self.analyze_dynamic_imports {
             return;
         }
 
-        let boxed_expr = match call_expr.callee.clone() {
-            Super(_) => return,
-            Expr(boxed) => boxed,
-        };
-
-        match &*boxed_expr {
-            Ident(ident) => {
-                if &ident.sym.to_string() != "import" {
+        match call_expr.callee.clone() {
+            Expr(box Ident(Ident { sym, .. })) =>
+            {
+                #[allow(clippy::cmp_owned)]
+                if sym.to_string() != "import" {
                     return;
                 }
             }
             _ => return,
         };
 
-        if let Some(arg) = call_expr.args.get(0) {
-            match &*arg.expr {
-                Lit(lit) => {
-                    if let Lit::Str(str_) = lit {
-                        let src_str = str_.value.to_string();
-                        self.dependencies.push(src_str);
-                    }
-                }
-                _ => return,
-            }
+        if let Some(ExprOrSpread {
+            expr: box Lit(Str(src)),
+            ..
+        }) = call_expr.args.get(0)
+        {
+            self.dependencies.push(src.value.to_string());
         }
     }
 }
@@ -105,7 +98,9 @@ import { foo } from "./foo.ts";
 export { bar } from "./foo.ts";
 export * from "./bar.ts";
 const a = await import("./fizz.ts");
-const a = await import("./" + "buzz.ts");
+const b = await import("./" + "buzz.ts");
+const c = await import("hello" + "world");
+const d = call("yo");
 "#;
 
     let dependencies = analyze_dependencies(source, true).expect("Failed to parse");
