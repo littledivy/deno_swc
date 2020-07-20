cfa3c9c7b200420da2ad5a79ce297c7c71c0105f
divy-work <dj.srivastava23@gmail.com>
2020-07-20 16:17:46 +0530

    fix: pass program instead of str

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use deno_core::plugin_api::ZeroCopyBuf;
 
 use serde::Deserialize;
 
-use core::{analyzer, parser};
+use core::{analyzer, parser, printer};
 
 #[no_mangle]
 pub fn deno_plugin_init(interface: &mut dyn Interface) {
@@ -37,6 +37,24 @@ fn ops_extract_dependencies(_interface: &mut dyn Interface, zero_copy: &mut [Zer
         Err(_) => {
             //TODO: return actual error message instead of "parse_error"
             let result = serde_json::to_string("parse_error").expect("failed to serialize Deps");
+            let result_box: Buf = serde_json::to_vec(&result).unwrap().into_boxed_slice();
+            Op::Sync(result_box)
+        }
+    };
+}
+
+fn op_print(_interface: &mut dyn Interface, zero_copy: &mut [ZeroCopyBuf]) -> Op {
+    let data = &zero_copy[0][..];
+    let prg: serde_json::Value = serde_json::from_slice(&data).unwrap();
+    return match printer::print(prg.to_string()) {
+        Ok(program) => {
+            let result = serde_json::to_string(&program).expect("failed to serialize Program");
+            let result_box: Buf = serde_json::to_vec(&result).unwrap().into_boxed_slice();
+            Op::Sync(result_box)
+        }
+        Err(e) => {
+            let result =
+                serde_json::to_string(&e.to_string()).expect("failed to serialize Program");
             let result_box: Buf = serde_json::to_vec(&result).unwrap().into_boxed_slice();
             Op::Sync(result_box)
         }
