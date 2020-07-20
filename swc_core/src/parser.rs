e928d8d40d671185d484878365b052dc30e8a08a
divy-work <dj.srivastava23@gmail.com>
2020-07-20 13:44:30 +0530

    feat: map err to string

--- swc_core/src/parser.rs
+++ swc_core/src/parser.rs
@@ -1,23 +1,8 @@
-use std::sync::Arc;
-use swc::common::{errors::Handler, FileName, FilePathMapping, SourceMap};
-use swc::config::ParseOptions;
-use swc::ecmascript::parser;
-use swc::Compiler;
+use crate::ast_parser;
 
-fn create_compiler() -> Compiler {
-    let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
-    let handler = Arc::new(Handler::with_tty_emitter(
-        swc::common::errors::ColorConfig::Always,
-        true,
-        false,
-        Some(cm.clone()),
-    ));
-    Compiler::new(cm, handler)
-}
-
-pub fn parse_ts(src: String) -> Result<swc::ecmascript::ast::Program, anyhow::Error> {
+pub fn parse(src: String) -> Result<swc_ecma_ast::Module, anyhow::Error> {
     let p = ast_parser::AstParser::new();
-    p.parse_module("root.ts", src, |parse_result| {
+    p.parse_module("root.ts", &src, |parse_result| {
         let module = parse_result?;
         Ok(module)
     })
