a3367ad44a28831c10feb93a6a1b974823d3d728
divy-work <dj.srivastava23@gmail.com>
2020-07-21 16:33:50 +0530

    feat: make parser accept the options

--- swc_core/src/parser.rs
+++ swc_core/src/parser.rs
@@ -1,8 +1,12 @@
 use crate::ast_parser;
+use crate::options::ParseOptions;
 
-pub fn parse(src: String) -> Result<swc_ecma_ast::Module, anyhow::Error> {
+pub fn parse(
+    src: String,
+    parse_options: ParseOptions,
+) -> Result<swc_ecma_ast::Module, anyhow::Error> {
     let p = ast_parser::AstParser::new();
-    p.parse_module("root.ts", &src, |parse_result| {
+    p.parse_module("root.ts", &src, parse_options, |parse_result| {
         let module = parse_result?;
         Ok(module)
     })
