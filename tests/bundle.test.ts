f519a4ab0ce91b9662ac59af16115d9f66b4b3a6
divy-work <dj.srivastava23@gmail.com>
2020-07-22 13:00:41 +0530

    feat!: finalize bundle tests

--- tests/bundle.test.ts
+++ tests/bundle.test.ts
@@ -3,6 +3,7 @@ import { assertEquals } from "./deps.ts";
 
 Deno.test("bundle (no error)", () => {
   const result = bundle({
-    entry: [],
+    entry: [Deno.cwd() + "/tests/test_files/bundle_me.ts"],
   });
+  assertEquals(result[Object.keys(result)[0]].code.trim(), "export var z = 3;");
 });
