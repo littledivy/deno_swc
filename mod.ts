14842f9e5585b2fcb6e66ac81144dc13d5eb170f
Dante Issaias <dante@issaias.com>
2022-07-12 17:36:47 +0100

    Fix types (#45)

--- mod.ts
+++ mod.ts
@@ -1,11 +1,10 @@
-import { instantiate } from "./lib/deno_swc.generated.js";
 import { decompress } from "https://deno.land/x/lz4@v0.1.2/mod.ts";
-import {
+import type {
   Config,
   ParseOptions,
   Program,
-  TransformConfig,
-} from "./types/options.ts";
+} from "https://esm.sh/@swc/core@1.2.212/types.d.ts";
+import { instantiate } from "./lib/deno_swc.generated.js";
 
 const { parseSync, printSync, transformSync } = await instantiate(decompress);
 
@@ -13,15 +12,10 @@ export function parse(source: string, opts: ParseOptions): Program {
   return parseSync(source, opts);
 }
 
-// TODO(littledivy): Typings for `program`
-// deno-lint-ignore no-explicit-any
-export function print(program: any, opts?: Config): { code: string } {
+export function print(program: Program, opts?: Config): { code: string } {
   return printSync(program, opts || {});
 }
 
-export function transform(
-  source: string,
-  opts: TransformConfig,
-): { code: string } {
+export function transform(source: string, opts: Config): { code: string } {
   return transformSync(source, opts);
 }
